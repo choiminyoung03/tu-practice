9693d921c4c88643d6966f727c5d567b28141944
choiminyoung03 <chlalsdlstj2@naver.com>
2025-06-09 18:01:28 +0900

    first commit

---
+++ src/main/java/com/framework/TuPracticeApplication.java
@@ -0,0 +1,14 @@
+package com.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuPracticeApplication.class, args);
+		System.out.println("Hello, tu-practice!");
+	}
+
+}
